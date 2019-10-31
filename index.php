4d4398ff20483216d7e540fdeabfc02e624022f9
Steven Jackson <stevenpjackson0@gmail.com>
2019-10-31 14:13:50 -0500

    Add: $list array

--- index.php
+++ index.php
@@ -2,4 +2,22 @@
 
 /* !Foreach Loops! */ 
 
+//Example array
+
+$list[] = [
+    'title' => 'Laundry',
+    'priority' => 2,
+    'due' => '',
+    'complete' => true,
+]; 
+
+$list[] = [
+    'title' => 'Clean out refrigerator',
+    'priority' => 3,
+    'due' => '07/30/2016',
+    'complete' => false,
+];
+
+var_dump($list);
+
 ?>
