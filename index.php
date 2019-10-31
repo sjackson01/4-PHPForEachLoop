83aac8d50f19ea03a45cad23ca70573fc5e28f3a
Steven Jackson <stevenpjackson0@gmail.com>
2019-10-31 14:31:59 -0500

    Edit: Comment

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 include ('list.php');
 
 //Echo value of 'title' on each iteration
+//Item becomes associative array with named key and value
 foreach($list as $item){
     echo $item['title'] . "<br /> \n";
 }
