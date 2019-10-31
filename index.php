237a7372455ed5f7bec55ab5aa27f127c6e590e8
Steven Jackson <stevenpjackson0@gmail.com>
2019-10-31 14:33:11 -0500

    Move: Comments

--- index.php
+++ index.php
@@ -3,10 +3,9 @@
 
 include ('list.php');
 
-//Echo value of 'title' on each iteration
-//Item becomes associative array with named key and value
-foreach($list as $item){
-    echo $item['title'] . "<br /> \n";
+
+foreach($list as $item){//Item becomes associative array with named key and value
+    echo $item['title'] . "<br /> \n";//Echo value of 'title' on each iteration
 }
 
 
