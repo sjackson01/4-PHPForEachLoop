773bf2eea99dad315b427efa3549a68074367ea0
Steven Jackson <stevenpjackson0@gmail.com>
2019-10-31 14:43:19 -0500

    Update: Foreach loop to display table

--- index.php
+++ index.php
@@ -2,11 +2,22 @@
 /* !Foreach Loops! */ 
 
 include ('list.php');
-
-
+/* Create Table */
+echo '<table>';
+echo '<tr>';//Table row
+echo '<th>Title</th>';//Table header
+echo '<th>Priority</th>';
+echo '<th>Due Date</th>';
+echo '<th>Complete</th>';
+echo '</th>';
 foreach($list as $item){//Item becomes associative array with named key and value
-    echo $item['title'] . "<br /> \n";//Echo value of 'title' on each iteration
+    echo '<tr>';//Each iteration add table row
+    echo /*Each iteration add table cell */ '<td>' . $item['title'] . "</td> \n";//Echo value of 'title' on each iteration
+    echo '<td>' . $item['priority'] . "<td> \n";
+    echo '<td>' . $item['due'] . "<td> \n";
+    echo '<td>' . $item['complete'] . "<td> \n";
+    echo '</tr>';
 }
-
+echo '</table>';
 
 ?>
